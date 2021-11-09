c8d5887f8a1e4d82bff25105bd729a1a3fb12cdf
chikardo1 <72710458+chikardo1@users.noreply.github.com>
2021-11-09 18:25:29 -0300

    Delete Angularapi/backend directory

--- Angularapi/backend/src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://chikardo:4455322@cluster0.ygczp.mongodb.net/angular-auth?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(db => console.log('Database is connected'))
-    .catch(err => console.log(err));
